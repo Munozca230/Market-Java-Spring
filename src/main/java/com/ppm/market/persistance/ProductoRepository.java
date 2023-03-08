7a3e01a49c97f019f8742e210d0c8b5e5399f64b
Jose Muñoz <miguejose230@gmail.com>
2023-03-08 10:12:28 -0500

    First commit

---
+++ src/main/java/com/ppm/market/persistance/ProductoRepository.java
@@ -0,0 +1,37 @@
+package com.ppm.market.persistance;
+
+import com.ppm.market.persistance.crud.ProductoCrudRepository;
+import com.ppm.market.persistance.entity.Producto;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository {
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll() {
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad, boolean estado) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
+    }
+}
---
+++ src/main/java/com/ppm/market/persistance/crud/ProductoCrudRepository.java
@@ -0,0 +1,12 @@
+package com.ppm.market.persistance.crud;
+
+import com.ppm.market.persistance.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}
